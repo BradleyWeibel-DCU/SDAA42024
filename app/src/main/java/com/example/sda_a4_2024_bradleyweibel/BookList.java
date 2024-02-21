0089be535067c25f501716d5bcb06aaf2cda049f
BradleyWeibel-DCU <bradley.weibel2@mail.dcu.ie>
2024-02-21 03:13:48 +0100

    8th Commit. Task 2 half complete. Firebase storage and realtime database is used to populate the book data in Book UI tab. Book covers removed from app. No more hard coded book data in app - all fetched from Google Firebase. Stable version.

--- app/src/main/java/com/example/sda_a4_2024_bradleyweibel/BookList.java
+++ app/src/main/java/com/example/sda_a4_2024_bradleyweibel/BookList.java
@@ -1,13 +1,20 @@
 package com.example.sda_a4_2024_bradleyweibel;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import java.util.ArrayList;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Images used are sourced from Public Domain Day 2019.
@@ -19,9 +26,14 @@ import java.util.ArrayList;
 
 public class BookList extends Fragment {
 
+    private static final String TAG = "BookList";
     public BookList() { } // Required empty public constructor
 
-    ViewPageAdapter adapter;
+    // Array for each value of a book
+    ArrayList<String> authorList = new ArrayList<>();
+    ArrayList<String> titleList = new ArrayList<>();
+    ArrayList<String> bookCoverURLList = new ArrayList<>();
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,39 +41,59 @@ public class BookList extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_book_list, container, false);
 
-        RecyclerView recyclerView = root.findViewById(R.id.bookView_view);
+        recyclerView = root.findViewById(R.id.bookView_view);
 
-        //add array for each item
-        ArrayList<String> mAuthor = new ArrayList<>();
-        ArrayList<String> mTitle = new ArrayList<>();
-        ArrayList<Integer> mImageID = new ArrayList<>();
+        // Get default FirebaseDatabase instance
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        // Get reference for database containing all library books and their info
+        DatabaseReference databaseLibraryReference = firebaseDatabase.getReference("Library_Books");
 
-        //simple loop here to add the images to the array without typing each one
-        for(int i=1;i<=14;i++) {
-            int id = getResources().getIdentifier("sku1000" + i, "drawable", root.getContext().getPackageName());
-            mImageID.add(id);
+        // Loop to add the each book's data to the array without typing each one
+        for (int i = 1; i <= 14; i++)
+        {
+            // Get book details at specific key - for example: sku10001
+            databaseLibraryReference.child("sku1000" + i).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task)
+                {
+                    if (task.isSuccessful())
+                    {
+                        if (task.getResult().exists())
+                        {
+                            Log.i(TAG, "------------------------- Successfully Read  -------------------------");
+                            // Get desired row/entry from Library_Books table
+                            DataSnapshot dataSnapshot = task.getResult();
+                            // Get cells in table entry
+                            authorList.add(String.valueOf(dataSnapshot.child("Author").getValue()));
+                            titleList.add(String.valueOf(dataSnapshot.child("Title").getValue()));
+                            bookCoverURLList.add(String.valueOf(dataSnapshot.child("Cover").getValue()));
+                            // Check if UI Book tab can be populated due to all data being present in lists
+                            populateBookList();
+                        }
+                        else
+                            Log.e(TAG, "------------------------- No entry exists -------------------------");
+                    }
+                    else
+                        Log.e(TAG, "------------------------- Failed to read -------------------------");
+                }
+            });
         }
-
-        //adding author and title. TODO no longer hard coded
-        mAuthor.add("Edgar Rice Burroughs"); mTitle.add("Tarzan and the Golden Lion");
-        mAuthor.add("Agatha Christie"); mTitle.add("The Murder on the Links");
-        mAuthor.add("Winston S. Churchill"); mTitle.add("The World Crisis");
-        mAuthor.add("E.e. cummings"); mTitle.add("Tulips and Chimneys");
-        mAuthor.add("Robert Frost"); mTitle.add("New Hampshire");
-        mAuthor.add("Kahlil Gibran"); mTitle.add("The Prophet");
-        mAuthor.add("Aldous Huxley"); mTitle.add("Antic Hay");
-        mAuthor.add("D.H. Lawrence"); mTitle.add("Kangaroo");
-        mAuthor.add("Bertrand and Dora Russell"); mTitle.add("The Prospects of Industrial Civilization");
-        mAuthor.add("Carl Sandberg"); mTitle.add("Rootabaga Pigeons");
-        mAuthor.add("Edith Wharton"); mTitle.add("A Son at the Front");
-        mAuthor.add("P.G. Wodehouse"); mTitle.add("The Inimitable Jeeves");
-        mAuthor.add("P.G. Wodehouse"); mTitle.add("Leave it to Psmith");
-        mAuthor.add("Viginia Woolf"); mTitle.add("Jacob's Room");
-
-        LibraryViewAdapter recyclerViewAdapter = new LibraryViewAdapter(getContext(), mAuthor, mTitle, mImageID);
-        recyclerView.setAdapter(recyclerViewAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
         return root;
+    }
+
+    // Only populate the Books tab when all Library_Books table data has been retrieved from the Firebase Realtime Database
+    private void populateBookList()
+    {
+        if (authorList.size() == 14 && titleList.size() == 14 && bookCoverURLList.size() == 14)
+        {
+            Log.i(TAG, "----------------------------- Ready to populate !!!!!!!!!!!!!");
+            LibraryViewAdapter recyclerViewAdapter = new LibraryViewAdapter(getContext(), authorList, titleList, bookCoverURLList);
+            recyclerView.setAdapter(recyclerViewAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            Log.i(TAG, "----------------------------- Finished populating !!!!!!!!!!!!!!!!!!!!!!!!!!");
+        }
+        else
+            Log.i(TAG, "----------------------------- NOT Ready to populate!");
     }
 }
--- app/src/main/java/com/example/sda_a4_2024_bradleyweibel/LibraryViewAdapter.java
+++ app/src/main/java/com/example/sda_a4_2024_bradleyweibel/LibraryViewAdapter.java
@@ -19,7 +19,6 @@ package com.example.sda_a4_2024_bradleyweibel;
 import static com.example.sda_a4_2024_bradleyweibel.Helper.showToast;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,29 +28,30 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import java.util.ArrayList;
 
 /*
  * @author Chris Coughlan 2019
  */
+
 public class LibraryViewAdapter extends RecyclerView.Adapter<LibraryViewAdapter.ViewHolder> {
 
     private static final String TAG = "RecyclerViewAdapter";
     private Context mNewContext;
+    // Arrays for each aspect of a book
+    private ArrayList<String> authorList, titleList, bookCoverUrlList;
 
-    //add array for each item\
-    private ArrayList<String> mAuthor;
-    private ArrayList<String> mTitle;
-    private ArrayList<Integer> mImageID;
-
-    LibraryViewAdapter(Context mNewContext, ArrayList<String> author, ArrayList<String> title, ArrayList<Integer> imageId) {
+    // Get context for Glide and lists containing string data
+    LibraryViewAdapter(Context mNewContext, ArrayList<String> author, ArrayList<String> title, ArrayList<String> bookCoverURL) {
         this.mNewContext = mNewContext;
-        this.mAuthor = author;
-        this.mTitle = title;
-        this.mImageID = imageId;
+        this.authorList = author;
+        this.titleList = title;
+        this.bookCoverUrlList = bookCoverURL;
     }
 
-    //declare methods
+    // Declare methods
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -60,18 +60,21 @@ public class LibraryViewAdapter extends RecyclerView.Adapter<LibraryViewAdapter.
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position)
+    {
+        // Populate this Book's details
+        // Get Author text
+        viewHolder.authorText.setText(authorList.get(position));
+        // Get Book title text
+        viewHolder.titleText.setText(titleList.get(position));
+        // Use Glide to get the image from the URL and insert it into the imageItem object
+        Glide.with(mNewContext).load(bookCoverUrlList.get(position)).apply(new RequestOptions()).into(viewHolder.imageItem);
 
-        Log.d(TAG, "onBindViewHolder: was called");
-        viewHolder.authorText.setText(mAuthor.get(position));
-        viewHolder.titleText.setText(mTitle.get(position));
-        viewHolder.imageItem.setImageResource(mImageID.get(position));
-
-        //should check here to see if the book is available.
+        // TODO should check here to see if the book is available.
         viewHolder.checkOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                showToast(mTitle.get(position), mNewContext);
+                showToast(titleList.get(position), mNewContext);
                 //...
                 Intent myOrder = new Intent (mNewContext, CheckOut.class);
                 mNewContext.startActivity(myOrder);
@@ -81,10 +84,10 @@ public class LibraryViewAdapter extends RecyclerView.Adapter<LibraryViewAdapter.
 
     @Override
     public int getItemCount() {
-        return mAuthor.size();
+        return authorList.size();
     }
 
-    //view holder class for recycler_list_item.xml
+    // Populate the UI elements using the view holder class for the UI side recycler_list_item.xml
     class ViewHolder extends RecyclerView.ViewHolder{
 
         ImageView imageItem;
